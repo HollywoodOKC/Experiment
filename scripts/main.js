bfff6556e685ac0ecc98e26eb85c5471920794b2
HollywoodOKC <T_Lane11@yahoo.com>
2021-05-03 19:15:52 -0500

    Initial push.

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+const myTime = () => {
+  const time = new Date();
+  const hour = time.getHours();
+  const min = time.getMinutes();
+  const sec = time.getSeconds();
+  const currentTime = `${hour}:${min}:${sec}`
+  document.getElementById("myClockDisplay").innerHTML = currentTime;
+  setTimeout(myTime, 1000);
+}
+myTime();
+
+function timeOfDay() {
+  const theTime = new Date();
+  const theHour = theTime.getHours();
+
+  if (theHour >= 0 && theHour <= 11) {
+    document.getElementById("timeOfDay").innerHTML = `Good Morning`;
+  } else if (theHour >= 12 && theHour <= 18) {
+    document.getElementById("timeOfDay").innerHTML = "Good Afternoon";
+  } else if (theHour >= 19 && theHour <= 23) {
+    document.getElementById("timeOfDay").innerHTML = "Good Evening";
+  } else {
+    document.getElementById("timeOfDay").innerHTML = "Good Day";
+  }
+  setTimeout(timeOfDay, 1000);
+}
+
+timeOfDay();
+
+document.getElementById("btn").addEventListener("click", function() {
+  document.getElementById("demo").innerHTML = "Test Succesful!";
+});
